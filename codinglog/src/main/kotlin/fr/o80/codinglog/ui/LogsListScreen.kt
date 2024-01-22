59aa728c45d6a4c6d229ef729ec4eb0a3c1b7ca3
Olivier PEREZ <olivier@olivierperez.fr>
2024-01-22 21:09:48 +0100

    Animate log cards

--- codinglog/src/main/kotlin/fr/o80/codinglog/ui/LogsListScreen.kt
+++ codinglog/src/main/kotlin/fr/o80/codinglog/ui/LogsListScreen.kt
@@ -1,24 +1,17 @@
 package fr.o80.codinglog.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.o80.codinglog.data.entity.LogInfoEntity
@@ -26,7 +19,6 @@ import fr.o80.codinglog.ui.model.CategoryUi
 import fr.o80.codinglog.ui.molecule.CategoryFilters
 import fr.o80.codinglog.ui.molecule.LogItem
 import fr.o80.codinglog.ui.theme.CodingLogThemePreview
-import java.text.SimpleDateFormat
 import java.util.Date
 
 @Composable
@@ -44,6 +36,7 @@ internal fun LogsListScreen(modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun LogsList(
     logs: List<LogInfoEntity>,
@@ -62,8 +55,17 @@ private fun LogsList(
                 onCategoryClick = onCategoryClick
             )
         }
-        items(logs) { log ->
-            LogItem(log, Modifier.fillMaxWidth())
+        items(
+            items = logs,
+            contentType = { "log" },
+            key = { it.uid }
+        ) { log ->
+            LogItem(
+                log = log,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateItemPlacement()
+            )
         }
     }
 }
